87769c695dbecad2b155c9312d32af1c26678400
GerbenVandeVelde <vandeveldegerben@gmail.com>
2025-01-13 09:42:04 +0100

    Update send_data_hackrf

--- python/failed scripts/send_data_hackrf.py
+++ python/failed scripts/send_data_hackrf.py
@@ -1,14 +1,29 @@
 import subprocess
 
+def manchester_encode(data):
+    encoded = ''
+    for bit in data:
+        if bit == '0':
+            encoded += '01'
+        else:
+            encoded += '10'
+    return encoded
+
 # TPMS packet in hex (Preamble + Sensor ID + Pressure + Temp + Status + CRC)
 tpms_packet_hex = "AA AA AA 82 D1 41 98 EC 08 01 4F"
 
 # Omzetten naar binaire data
-tpms_packet_bytes = bytes.fromhex(tpms_packet_hex.replace(" ", ""))
+binary_data = bin(int(tpms_packet_hex.replace(" ", ""), 16))[2:].zfill(len(tpms_packet_hex.replace(" ", "")) * 4)
+
+# Manchester encoderen
+encoded_data = manchester_encode(binary_data)
+
+# Omzetten naar bytes
+encoded_bytes = int(encoded_data, 2).to_bytes((len(encoded_data) + 7) // 8, byteorder='big')
 
 # Opslaan als binair bestand
 with open("tpms_signal.bin", "wb") as f:
-    f.write(tpms_packet_bytes)
+    f.write(encoded_bytes)
 
 # HackRF configuratie: 433.92 MHz, 250 kHz bandwidth
 subprocess.run([
